3e67472e67143928aaff103552e3cdffe9e87b3e
oliver <feketeoliver2006@gmail.com>
2025-09-18 11:07:04 +0200

    done

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Squarecomponent from './components/Squarecomponent'
+
+function App(){
+
+  return (
+      <Squarecomponent />
+  )
+}
+
+export default App;
---
+++ src/components/BoxComponent.tsx
@@ -0,0 +1,15 @@
+interface BoxProps{
+    color: string;
+    count : number;
+    setFn: () => void;
+}
+const BoxComponent : React.FC<BoxProps> = ({color,count , setFn}) => {
+    return (
+        <div className="box" style={{width: "10rem", height:"10rem", boxShadow:"0 0 10px 3px white inset", borderRadius: "7px", background:`${color}`, alignContent:"center", justifyContent:"center"}}
+        onClick={setFn}
+        >
+        {count}
+        </div>
+    )
+}
+export default BoxComponent;
