c018830fd68bda22af329c9d523f46ee5a8589b3
oliver <feketeoliver2006@gmail.com>
2025-09-23 10:19:42 +0200

    done

--- src/components/BoxComponent.tsx
+++ src/components/BoxComponent.tsx
@@ -5,7 +5,7 @@ interface BoxProps{
 }
 const BoxComponent : React.FC<BoxProps> = ({color,count , setFn}) => {
     return (
-        <div className="box" style={{width: "10rem", height:"10rem", boxShadow:"0 0 10px 3px white inset", borderRadius: "7px", background:`${color}`, alignContent:"center", justifyContent:"center"}}
+        <div className="box" style={{width: "10rem", height:"10rem", boxShadow:"0 0 10px 3px white inset", borderRadius: "7px", background:`${color}`, alignContent:"center", justifyContent:"center", userSelect:"none", cursor:"pointer"}}
         onClick={setFn}
         >
         {count}
--- src/components/Squarecomponent.tsx
+++ src/components/Squarecomponent.tsx
@@ -1,44 +1,48 @@
 import BoxComponent from "./BoxComponent";
 import { useState } from "react";
 
-const Squarecomponent = () => {
+const randomNum = () => Math.floor(Math.random()*256);
 
-    const [color,setcolor] = useState<string>("rgb(0,0,0)");
-    const [count, setcount] = useState(0);
+const resetColor = () => "#000";
 
-    const randomNum = () => Math.floor(Math.random()*256);
-    const resetColor = () => "#000";
-    const randomcolor = () => {
+const randomcolor = () => {
         const r = randomNum()
         const g = randomNum()
         const b = randomNum()
         return `rgb(${r},${g},${b})`
-    }
+}
 
-    const setBoxColor = () => setcolor(randomcolor());
-    const setBoxCount = () => {
-        setcount(prev => prev === 10 ? 0 : prev+1)
-    }
+const Squarecomponent = () => {
+
+   /*  const [color,setcolor] = useState<string>("rgb(0,0,0)");
+    const [count, setcount] = useState(0);
+ */
+    const [box, setBox] = useState({bgColor: "rgb(0,0,0)", bgCount:0});
+
+    
+
+    const setBoxColor = () => setBox(prev => ({...prev, bgColor:randomcolor()}));
+    const setBoxCount = () => setBox(prev => ({...prev, bgCount : prev.bgCount === 10 ? 0 : prev.bgCount +1}));
 
     return(
         <main style={{display: "flex", flexDirection: "column", alignItems:"center", justifyContent: "center", gap:"2rem"}}>
-            <h3>A szín kódja: {color}</h3>
+            <h3>A szín kódja: {box.bgColor}</h3>
 
             {/*<div className="box" style={{width: "10rem", height:"10rem", boxShadow:"0 0 10px 3px white inset",      borderRadius: "7px", background:`${color}`, alignContent:"center", justifyContent:"center"}} 
             onClick={() => setcount(prev => prev +1)}  
             >{count}</div> */}
 
             <div style={{display: "flex", flexDirection: "row", alignItems:"center", justifyContent: "center",gap:"2rem"}}>
-                <BoxComponent color={color} count={count} setFn={setBoxCount} />
-                <BoxComponent color={color} count={count} setFn={setBoxCount} />
-                <BoxComponent color={color} count={count} setFn={setBoxCount} />
+                <BoxComponent  color={box.bgColor} count={box.bgCount} setFn={setBoxCount} />
+                <BoxComponent color={box.bgColor} count={box.bgCount} setFn={setBoxCount} />
+                <BoxComponent color={box.bgColor} count={box.bgCount} setFn={setBoxCount} />
             </div>
 
             <button onClick={setBoxColor}>Színezz!</button>
 
             <button onClick={()=> {
-                setcolor(resetColor())
-                setcount(0)
+                setBox(prev => ({...prev, bgColor: resetColor()}));
+                setBox(prev => ({...prev, bgCount: 0}));
             }}>Reset</button>
 
         </main>
